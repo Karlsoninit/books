d58e6e53261d9ea5084f24a76ee3ad76e7dabeaa
Maksim <maksim@MacBook-Pro-Maksim.local>
2024-11-08 16:21:49 +0200

    connected open modal logic

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import {observer} from 'mobx-react-lite';
 import {useBooks} from "../../hooks/useBooks";
 import {BookList} from "../BookList/BookList";
+import {Modal} from "../../uikit/modal/Modal";
+import {BookForm} from "../BookForm/BookForm";
+import {BookType} from "../../types/Book.types";
 
 
 export const Book: React.FC = observer(() => {
-    const {books, isLoading} = useBooks()
+    const [isOpen, setIsOpen] = React.useState(false)
+    const {books, isLoading, addBook} = useBooks()
+
+    const handleOpenModal = () => {
+        setIsOpen((prev) => !prev);
+    }
 
     if (isLoading) return <p>Loading...</p>;
 
     return (
         <>
             <BookList books={books}/>
+            {isOpen &&
+                <Modal onClose={handleOpenModal}><BookForm createBook={addBook} handleClose={handleOpenModal}/></Modal>}
         </>
     )
 });
